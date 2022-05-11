ab30ab1c84f5ab24748f839545ea2546946a75fc
ravindrapalaskar <105346461+ravindrapalaskar@users.noreply.github.com>
2022-05-11 18:33:30 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,11 @@
+public class Student{
+  
+  public static void main(String[] args)
+  {
+    
+    System.out.println(" My 1st File created in Github");
+    System.out.println(" I will pull this in my working Directory");
+    
+  }
+  
+}
